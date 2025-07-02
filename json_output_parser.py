7cc2df75592003b72874b4f76aae81e08912d9aa
Rohan18M20 <rajarohansinha@gmail.com>
2025-07-02 15:55:01 +0530

    Add files via upload

---
+++ json_output_parser.py
@@ -0,0 +1,34 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+load_dotenv()
+
+# Define the model
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+parser = JsonOutputParser()
+
+# template = PromptTemplate(
+#     template='What is the name and age of indian cricket captain \n {format_instruction}',
+#     input_variables=[],
+#     partial_variables={'format_instruction': parser.get_format_instructions()}
+# )
+
+template = PromptTemplate(
+    template='Give me 5 facts about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction': parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic' : 'India'})
+
+print(result)
